11e95a1ecb5c50f3e38d3a70473ed9c8cfcf8817
BrandonAllan <allanbrandon34@gmail.com>
2026-05-02 00:25:23 +0300

    Set up MuJoCo enviroment and robot model

--- drl_training/src/test_sim.py
+++ drl_training/src/test_sim.py
@@ -4,7 +4,7 @@ import time
 
 # 1. Point to the specific robot model you want to load
 # Let's start with the quadruped for the thesis!
-model_path = "drl_training/models/universal_robots_ur5e/scene.xml"
+model_path = "drl_training/models/robot.xml"
 
 print(f"Loading model from: {model_path}")
 
